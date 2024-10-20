2651d97db42224aa8258b74c228d792f26910b2d
nazaninshah <84009690+nazaninshah@users.noreply.github.com>
2024-10-21 00:37:17 +0330

    Delete spring-multiThread-pro/spring-multiThread-pro directory

--- spring-multiThread-pro/spring-multiThread-pro/src/main/java/com/project/shahbazi/spring_multiThread_pro/repository/AccountRepo.java
+++
@@ -1,7 +0,0 @@
-package com.project.shahbazi.spring_multiThread_pro.repository;
-
-import com.project.shahbazi.spring_multiThread_pro.entity.AccountEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepo extends JpaRepository<AccountEntity, Long> {
-}
